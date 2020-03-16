55cb72f5476dd1faac4a6513f5c7304e226c7814
Cristian Aldea <aldea.cristian@outlook.com>
2020-03-15 22:12:04 -0400

    Update Doc

--- custom_tokenize.py
+++ custom_tokenize.py
@@ -5,9 +5,10 @@ from constants import OUT_OF_VOCABULARY_DELIM
 
 def tokenize(tweet: str, span: int) -> List[str]:
     """
-    Tokenize tweets by a single legal character
-    :param span: Execution range. Ex: Unigram = 1; Bigram = 2; Trigram = 3
-    :param tweet: Tweet input
+    Tokenize tweets given a length for the tokens.
+
+    :param span: Execution range. Ex: Unigram = 1 character; Bigram = 2 characters; Trigram = 3 characters
+    :param tweet: Tweet input string
     :return: List of tokens
     """
     tweet_len = len(tweet)
